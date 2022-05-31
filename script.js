6b1630dee18a8d2d5c5e9ea9fb280384167d5252
Zarin Salim <zarin_96@hotmail.co.uk>
2022-05-31 16:16:06 +0100

    adding Jordan's code

--- script.js
+++ script.js
@@ -1,42 +1,75 @@
-let sqName = document.querySelector(".squad-name");
-let home = document.querySelector(".home-town");
-let formed = document.querySelector(".formed");
-let secretBase = document.querySelector(".secretBase");
-// let members = document.querySelector(".members");
-let membersName = document.querySelector(".members-name");
-let membersAge = document.querySelector(".members-age");
-let membersSecret = document.querySelector(".members-secret");
-let membersPowers = document.querySelector(".members-powers");
+// let sqName = document.querySelector(".squad-name");
+// let home = document.querySelector(".home-town");
+// let formed = document.querySelector(".formed");
+// let secretBase = document.querySelector(".secretBase");
+// let membersName0 = document.querySelector(".members-name");
+// let membersAge0 = document.querySelector(".members-age");
+// let membersSecret0 = document.querySelector(".members-secret");
+// let membersPowers0 = document.querySelector(".members-powers");
+
+// axios.get('https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/example.json')
+//     .then(response => {
+//         const heroes = response.data;
+//         console.log(heroes);
+
+//         sqName.innerHTML = heroes.squadName;
+//         home.innerHTML = heroes.homeTown;
+//         formed.innerHTML = heroes.formed;
+//         secretBase.innerHTML = heroes.secretBase;
+
+//         membersName0.innerHTML = heroes.members[0].name;
+//         membersAge0.innerHTML = heroes.members[0].age;
+//         membersSecret0.innerHTML = heroes.members[0].secretIdentity;
+
+//         membersPowers0.innerHTML = heroes.members[0].powers;
 
 
+//     }).catch(err => console.error(err));
 
-axios.get('https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/example.json')
-    .then(function (response) {
-        console.log(response);
+const output = document.getElementById('output');
 
-        sqName.innerHTML = response.data.squadName;
-        home.innerHTML = response.data.homeTown;
-        formed.innerHTML = response.data.formed;
-        secretBase.innerHTML = response.data.secretBase;
 
-        let members = [];
+axios.get("https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/example.json")
+    .then(response => {
+        const heroes = response.data;
+        console.log("HEROES: ", heroes);
 
-        for (var i = 0; i < members.length; i++) {
-            console.log(members);
+        const squadName = document.createElement("h1");
+        squadName.innerText = heroes.squadName;
+        output.appendChild(squadName);
 
-            // members.innerHTML = response.data.members;
-            membersName.innerHTML = response.data.members.name;
-            membersAge.innerHTML = response.data.members.age;
-            membersSecret.innerHTML = response.data.members.secretIdentity;
-            membersPowers.innerHTML = response.data.members.powers;
+        const secretBase = document.createElement("p");
+        secretBase.innerText = "Secret base: " + heroes.secretBase;
+        output.appendChild(secretBase);
+
+        const homeTown = document.createElement("p");
+        homeTown.innerText = "Hometown: " + heroes.homeTown;
+        output.appendChild(homeTown);
+
+        const formed = document.createElement("p");
+        formed.innerText = "Formed: " + heroes.formed;
+        output.appendChild(formed);
+
+
+        for (let member of heroes.members) {
+            console.log("MEMBER: ", member);
+            const memberSection = document.createElement("section");
+
+            const memberName = document.createElement("h2");
+            memberName.innerText = member.name;
+            memberSection.appendChild(memberName);
+
+            const powers = document.createElement("ul");
+
+            for (let power of member.powers) {
+                const p = document.createElement("li");
+                p.innerText = power;
+                powers.appendChild(p);
+            }
+
+            memberSection.appendChild(powers);
+
+            output.appendChild(memberSection);
         }
 
-
-
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
-    .then(function () {
-
-    });
+    }).catch(err => console.error(err));
